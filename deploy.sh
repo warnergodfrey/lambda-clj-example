0d8412d88607cebf0b76c25407410f9f882674aa
Warner Godfrey <warner@godfrey.io>
2015-08-06 11:10:34 +1000

    deploy api gateway using warnergodfrey/aws-apigateway-swagger container

--- deploy.sh
+++ deploy.sh
@@ -15,3 +15,5 @@ aws lambda create-function \
   --timeout 10 \
   --role arn:aws:iam::$AWS_ACCOUNT_ID:role/lambda_exec_role \
   --zip-file fileb://./target/lambda-clj-examples-0.1.0-standalone.jar
+
+docker run -i -t -v $(pwd)/resources/swagger.json:/var/app/swagger.json -v ~/.aws:/root/.aws warnergodfrey/aws-apigateway-swagger -c /var/app/swagger.json
